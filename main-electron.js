927a327f059ab236ea7e99545b87c80bcb625baf
REINChat Developer <dev@reinchat.local>
2026-07-31 20:04:33 +0900

    Configure Electron entrypoint and build files

--- main-electron.js
+++ main-electron.js
@@ -1,6 +1,8 @@
-import { app, BrowserWindow } from 'electron';
+import pkg from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+const { app, BrowserWindow } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
